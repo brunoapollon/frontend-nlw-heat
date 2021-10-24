669c47a42baeca05c46b046a36d82feb48fc9e1a
Bruno Lopes <eusoubruno7@gmail.com>
2021-10-23 23:16:41 -0300

    feat: creating messageList component

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,3 +1,49 @@
+import styles from './styles.module.scss';
+
+import logoImg from '../../assets/logo.svg';
+
 export function MessageList() {
-  return <h1>MessageList</h1>;
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile2021 2021" />
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Texto texto texto textp</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/brunoapollon.png"
+                alt=" Bruno lopes "
+              />
+            </div>
+            <span>Bruno Lopes</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Texto texto texto textp</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/brunoapollon.png"
+                alt=" Bruno lopes "
+              />
+            </div>
+            <span>Bruno Lopes</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Texto texto texto textp</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/brunoapollon.png"
+                alt=" Bruno lopes "
+              />
+            </div>
+            <span>Bruno Lopes</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
